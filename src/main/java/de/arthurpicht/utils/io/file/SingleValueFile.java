887de8c4bce77a5bb7d5388ca9a397d722830309
arthurpicht <info@arthurpicht.de>
2021-06-07 10:04:56 +0200

    SingleValueFile, tests, prepared for version 0.0.2

---
+++ src/main/java/de/arthurpicht/utils/io/file/SingleValueFile.java
@@ -0,0 +1,67 @@
+package de.arthurpicht.utils.io.file;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Simple functionality for reading and writing a string to/from a file.
+ * Implementation is thread save. Existence of parent directory is assumed.
+ */
+public class SingleValueFile {
+
+    private final Path path;
+    private final Charset charset;
+
+    public SingleValueFile(Path path) {
+        this.path = path;
+        this.charset = StandardCharsets.UTF_8;
+    }
+
+    public SingleValueFile(Path path, Charset charset) {
+        this.path = path;
+        this.charset = charset;
+    }
+
+    public synchronized void write(String string) throws IOException {
+        Files.writeString(this.path, string, this.charset, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    /**
+     * Returns content from file as string. Existence of file is a precondition.
+     * If checking for file existence and reading can not be performed as an thread save (atomic) operation,
+     * catch IllegalStateException.
+     *
+     * @return content from file as String.
+     * @throws IOException
+     */
+    @SuppressWarnings("JavaDoc")
+    public synchronized String read() throws IOException {
+        if (!exists()) throw new IllegalStateException("No such file to read from: " + this.path.toString());
+        return Files.readString(this.path, this.charset);
+    }
+
+    /**
+     * Deletes file. Existence of file is a precondition.
+     *
+     * @throws IOException
+     */
+    @SuppressWarnings("JavaDoc")
+    public synchronized void delete() throws IOException {
+        if (!exists()) throw new IllegalStateException("No such file to delete: " + this.path.toString());
+        Files.delete(this.path);
+    }
+
+    public synchronized void deleteIfExists() throws IOException {
+        Files.deleteIfExists(this.path);
+    }
+
+    public synchronized boolean exists() {
+        return Files.exists(this.path);
+    }
+
+
+}
---
+++ src/test/java/de/arthurpicht/utils/io/file/SingleValueFileTest.java
@@ -0,0 +1,119 @@
+package de.arthurpicht.utils.io.file;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleValueFileTest {
+
+    // If a test fails, delete TEMP_TEST_DIR and all its content before rerunning!
+
+    private static final String TEMP_TEST_DIR = "tempTest";
+
+    @BeforeAll
+    static void createTempDir() throws IOException {
+        if (!Files.exists(Paths.get(TEMP_TEST_DIR))) {
+            Files.createDirectory(Paths.get(TEMP_TEST_DIR));
+        }
+    }
+
+    @AfterAll
+    static void deleteTempDir() throws IOException {
+        Files.deleteIfExists(Paths.get(TEMP_TEST_DIR));
+    }
+
+    @Test
+    void simple() throws IOException {
+
+        String test = "test";
+        Path path = Paths.get(TEMP_TEST_DIR, "test.txt");
+
+        SingleValueFile singleValueFile = new SingleValueFile(path);
+        singleValueFile.write(test);
+
+        String retest = singleValueFile.read();
+        assertEquals(test, retest);
+
+        singleValueFile.delete();
+        assertFalse(Files.exists(path));
+    }
+
+    @Test
+    void rewrite() throws IOException {
+
+        String test = "test";
+        Path path = Paths.get(TEMP_TEST_DIR, "test2.txt");
+
+        SingleValueFile singleValueFile = new SingleValueFile(path);
+        singleValueFile.write("0000000000000");
+        singleValueFile.write(test);
+
+        String retest = singleValueFile.read();
+        assertEquals(test, retest);
+
+        singleValueFile.delete();
+        assertFalse(Files.exists(path));
+    }
+
+    @Test
+    void exists() throws IOException {
+        Path path = Paths.get(TEMP_TEST_DIR, "exist.txt");
+        SingleValueFile singleValueFile = new SingleValueFile(path);
+        assertFalse(singleValueFile.exists());
+        singleValueFile.write("something");
+        assertTrue(singleValueFile.exists());
+        singleValueFile.delete();
+        assertFalse(singleValueFile.exists());
+    }
+
+    @Test
+    void readFromNonExistingFile() throws IOException {
+        Path path = Paths.get(TEMP_TEST_DIR, "not_existing.txt");
+        SingleValueFile singleValueFile = new SingleValueFile(path);
+        try {
+            singleValueFile.read();
+            fail("IllegalStateException expected.");
+        } catch (IllegalStateException e) {
+            // intentionally do nothing
+        }
+    }
+
+    @Test
+    void simpleISO() throws IOException {
+        //noinspection SpellCheckingInspection
+        String test = "testöäü";
+        Path path = Paths.get(TEMP_TEST_DIR, "test.txt");
+
+        SingleValueFile singleValueFile = new SingleValueFile(path, StandardCharsets.ISO_8859_1);
+        singleValueFile.write(test);
+
+        String retest = singleValueFile.read();
+        assertEquals(test, retest);
+
+        singleValueFile.delete();
+        assertFalse(Files.exists(path));
+    }
+
+    @Test
+    void deleteIfExists() throws IOException {
+        String test = "test";
+        Path path = Paths.get(TEMP_TEST_DIR, "test.txt");
+
+        SingleValueFile singleValueFile = new SingleValueFile(path, StandardCharsets.ISO_8859_1);
+        singleValueFile.write(test);
+
+        singleValueFile.deleteIfExists();
+
+        assertFalse(Files.exists(path));
+    }
+
+
+}
